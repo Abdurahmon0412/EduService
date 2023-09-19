d96df96f8fdb9aa4d508459be23e1de1a8cfb00c
Abdurahmon <abdurahmonsadriddinov0412@gmail.com>
2023-09-19 18:19:24 +0500

    firs commit

---
+++ EduServices/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduServices.Models
+{
+    public class Student
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public long SubjectId { get; set; }
+        public string Email { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set;}
+
+    }
+}
